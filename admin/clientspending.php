ff5e65ffc10b13d157dfe1ae17a5d9d2eab94ec1
root <developer.webservice@gmail.com>
2015-09-11 07:21:54 -0400

    Update permission and fix the messages if the Administrator allow or not

--- admin/clientspending.php
+++ admin/clientspending.php
@@ -16,27 +16,27 @@ require_once("../includes/csa-functions.php");
 if($_SESSION['mainadmin'] != "1") {
 	if(!in_array("addclient", $_SESSION['permissions']) && !in_array("editclient", $_SESSION['permissions']) && !in_array("deleteclient", $_SESSION['permissions']) && !in_array("manageclient", $_SESSION['permissions'])) {
 		$_SESSION['errormessage'] = $lang['nopermission'];
-		header("Location: index.php");
+		header("Location: dashboard.php");
 		exit();
 	}
 	if($_REQUEST['mode'] == "edit" && !in_array("editclient", $_SESSION['permissions'])) {
 		$_SESSION['errormessage'] = $lang['nopermission'];
-		header("Location: index.php");
+		header("Location: dashboard.php");
 		exit();
 	}
 	if($_REQUEST['mode'] == "add" && !in_array("addclient", $_SESSION['permissions'])) {
 		$_SESSION['errormessage'] = $lang['nopermission'];
-		header("Location: index.php");
+		header("Location: dashboard.php");
 		exit();
 	}
 	if($_REQUEST['mode'] == "delete" && !in_array("deleteclient", $_SESSION['permissions'])) {
 		$_SESSION['errormessage'] = $lang['nopermission'];
-		header("Location: index.php");
+		header("Location: dashboard.php");
 		exit();
 	}	
 	if($_REQUEST['mode'] == "approve" && !in_array("approveclient", $_SESSION['permissions'])) {
 		$_SESSION['errormessage'] = $lang['nopermission'];
-		header("Location: index.php");
+		header("Location: dashboard.php");
 		exit();
 	}
 }
